8b68ea7b3818b4f0f9b332b1bfd8d811f6c9539c
Misha V <misha@mv.home>
2016-03-28 11:30:53 -0400

    Updated all
    Updated items

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-$('#product, #service').click(function(){
-  $('#question-2').fadeIn();
-});
+$(document).ready(function(){
 
-$('#yes, #no').click(function() {
-  $('#question-3').fadeIn();
+	$('#product').click(function(){
+	  $('#question-2').fadeIn();
+	});
+	
+	$('#yes, #no').click(function() {
+	  $('#question-3').fadeIn();
+	});
+	
+	#('#fast').click(function(){
+	  $('#c-corp').fadeIn();
+	});
+	
+	#('#steady').click(function() {
+	  $('llc').fadeIn();
+	});
 });
-
-#('#fast').click(function(){
-  $('#c-corp').fadeIn();
-});
-
-#('#steady').click(function() {
-  $('llc').fadeIn();
-});
-
-// $('#fast, #steady').click(function() {
-//   if (true) {
-//
-//   }
-//
-//
-// });
